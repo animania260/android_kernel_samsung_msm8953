b85533b6b4740cf0fddfbb76f0d1823567dd3285
minz1 <emerytang@gmail.com>
2017-10-30 06:45:20 -0500

    drivers: firmware: add samsung changes

--- commit.sh
+++ commit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 for ARG in "$@"
-	git add drivers/$ARG && git commit -a -m "drivers: $ARG: add samsung changes"
+do
+	git checkout aef466ac5aefd085944ef89cb215b42dfab9904f drivers/$ARG
+	git add drivers/$ARG
+	git commit -a -m "drivers: $ARG: add samsung changes"
 done
